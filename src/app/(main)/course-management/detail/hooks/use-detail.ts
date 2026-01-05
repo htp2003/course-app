c427846847d9420efdd2a0a879fa58735590a4fe
htp2003 <hatanphat08102003@gmail.com>
2026-01-05 17:00:54 +0700

    fix parse courseId

--- src/app/(main)/course-management/detail/hooks/use-detail.ts
+++ src/app/(main)/course-management/detail/hooks/use-detail.ts
@@ -3,10 +3,12 @@ import { getCourseDetailRealAPI } from "../services/api";
 import { mapApiToUiForm } from "../../common/utils/mapper";
 
 export const useCourseDetail = (courseId: string | undefined) => {
+  const parsedId = courseId ? Number(courseId) : 0;
+
   return useQuery({
-    queryKey: ["course-detail-view", courseId],
-    queryFn: () => getCourseDetailRealAPI(courseId!),
-    enabled: !!courseId,
+    queryKey: ["course-detail-view", parsedId],
+    queryFn: () => getCourseDetailRealAPI(parsedId),
+    enabled: !!courseId && !isNaN(parsedId),
     select: (response) => mapApiToUiForm(response.data),
     retry: 1,
   });
